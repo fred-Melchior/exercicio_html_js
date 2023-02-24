b7ee7bb72906c1cdd6c281d6b128594ea5a35475
Fred Melchior <fred@MacBook-Air-de-Fred.local>
2023-02-24 11:52:02 -0300

    Atualicao do código e nova funcao de reset

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const form = document.getElementById('form-valores');
 
 function validaValores(valorA, valorB){
-    return valorA.value > valorB.value;
+    return valorA > valorB;
 }
 
 form.addEventListener('submit', function(e){
     let valorValido = false;
     e.preventDefault();
 
-    const valorA = document.getElementById('campoA')
-    const valorB = document.getElementById('campoB')
-
+    const valorA = parseInt(document.getElementById('campoA').value);
+    const valorB = parseInt(document.getElementById('campoB').value);
+    
     valorValido = validaValores(valorA, valorB)
     if (valorValido){
         let msgSucesso = document.querySelector('.msgSucesso')
         msgSucesso.style.display = 'block';
-        msgErro.style.display = 'none';
     }
     else{
         let msgErro = document.querySelector('.msgErro')
         msgErro.style.display = 'block';
     }
+})
+
+form.addEventListener('reset', function(e){
+
+    let msgSucesso = document.querySelector('.msgSucesso')
+    msgSucesso.style.display = 'none';
+    let msgErro = document.querySelector('.msgErro')
+    msgErro.style.display = 'none';
 })
